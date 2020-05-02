b342279c708aa4266b410213a8afb8801388919a
Squantor <squantmuts@gmail.com>
2020-05-02 23:08:32 +0200

    started with UART example

---
+++ playgrounds/LPC812_uart_example/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * This is simple example file which does do any I/O and thus
+ * can be compiled and run on any Cortex-M MCU. However, to see
+ * its effect, you'll need to use debugger.
+ */
+#include <mcu_ll.h>
+#include <board.hpp>
+int main()
+{
+    boardInit();
+    while (1) {
+        __NOP();
+    }
+}
