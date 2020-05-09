a90f58640c9e5c9f219568403530643cef674532
Squantor <squantmuts@gmail.com>
2020-05-09 13:33:36 +0200

    added generic CortexM3 target

--- examples/build.sh
+++ examples/build.sh
@@ -3,6 +3,8 @@ make -j18 PROJECT=CortexM0_example config=release
 make -j18 PROJECT=CortexM0_example config=debug
 make -j18 PROJECT=CortexM0plus_example config=release
 make -j18 PROJECT=CortexM0plus_example config=debug
+make -j18 PROJECT=CortexM3_example config=release
+make -j18 PROJECT=CortexM3_example config=debug
 make -j18 PROJECT=LPC812_example config=release
 make -j18 PROJECT=LPC812_example config=debug
 make -j18 PROJECT=LPC824_example config=release
