dbb38d31d5ea289b283e851bac069cd04da630e9
Squantor <squantmuts@gmail.com>
2020-05-09 13:40:39 +0200

    added generic CortexM4 example

--- examples/build.sh
+++ examples/build.sh
@@ -1,3 +1,4 @@
+# build all the examples and clean afterwards to quick check builds
 #!/bin/bash
 make -j18 PROJECT=CortexM0_example config=release
 make -j18 PROJECT=CortexM0_example config=debug
@@ -5,6 +6,8 @@ make -j18 PROJECT=CortexM0plus_example config=release
 make -j18 PROJECT=CortexM0plus_example config=debug
 make -j18 PROJECT=CortexM3_example config=release
 make -j18 PROJECT=CortexM3_example config=debug
+make -j18 PROJECT=CortexM4_example config=release
+make -j18 PROJECT=CortexM4_example config=debug
 make -j18 PROJECT=LPC812_example config=release
 make -j18 PROJECT=LPC812_example config=debug
 make -j18 PROJECT=LPC824_example config=release
