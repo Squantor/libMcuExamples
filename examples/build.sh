c22b3d55bfa4847bb1c28752d1579acb21a71dea
Squantor <squantmuts@gmail.com>
2020-06-24 23:24:47 +0200

    fixed typo in build test script

--- examples/build.sh
+++ examples/build.sh
@@ -1,15 +1,15 @@
 # build all the examples and clean afterwards to quick check builds
 #!/bin/bash
-make -j18 PROJECT=CortexM0_example config=release
-make -j18 PROJECT=CortexM0_example config=debug
-make -j18 PROJECT=CortexM0plus_example config=release
-make -j18 PROJECT=CortexM0plus_example config=debug
-make -j18 PROJECT=CortexM3_example config=release
-make -j18 PROJECT=CortexM3_example config=debug
-make -j18 PROJECT=CortexM4_example config=release
-make -j18 PROJECT=CortexM4_example config=debug
-make -j18 PROJECT=LPC812_example config=release
-make -j18 PROJECT=LPC812_example config=debug
-make -j18 PROJECT=LPC824_example config=release
-make -j18 PROJECT=LPC824_example config=debug
+make -j18 PROJECT=CortexM0_example CONFIG=release
+make -j18 PROJECT=CortexM0_example CONFIG=debug
+make -j18 PROJECT=CortexM0plus_example CONFIG=release
+make -j18 PROJECT=CortexM0plus_example CONFIG=debug
+make -j18 PROJECT=CortexM3_example CONFIG=release
+make -j18 PROJECT=CortexM3_example CONFIG=debug
+make -j18 PROJECT=CortexM4_example CONFIG=release
+make -j18 PROJECT=CortexM4_example CONFIG=debug
+make -j18 PROJECT=LPC812_example CONFIG=release
+make -j18 PROJECT=LPC812_example CONFIG=debug
+make -j18 PROJECT=LPC824_example CONFIG=release
+make -j18 PROJECT=LPC824_example CONFIG=debug
 make -j18 PROJECT=CortexM0_example clean
