2cbf1ac5bb788ec7f469d2feb61915aac163c91b
Squantor <squantmuts@gmail.com>
2020-07-14 22:33:00 +0200

    ported over blinky match to libmcull

--- examples/LPC812_sct_example/src/blinky_match.cpp
+++ examples/LPC812_sct_example/src/blinky_match.cpp
@@ -30,7 +30,6 @@ SOFTWARE.
 
 void setupSct(void)
 {
-    SctInit(LPC_SCT);
     // setup SCT out0 to pin 17
     ClockEnablePeriphClock(SYSCTL_CLOCK_SWM);
     ClockEnablePeriphClock(SYSCTL_CLOCK_IOCON);
@@ -38,15 +37,33 @@ void setupSct(void)
     SwmMovablePinAssign(SWM_CTOUT_0_O, PIN_LED);
     ClockDisablePeriphClock(SYSCTL_CLOCK_IOCON);
     ClockDisablePeriphClock(SYSCTL_CLOCK_SWM);
-    
-    LPC_SCT->CONFIG |= 1;
-    LPC_SCT->MATCHREL[0].U = ClockGetSystemClockRate();
-    LPC_SCT->EV[0].STATE = (1 << 0);
-    LPC_SCT->EV[0].CTRL = (0 << 0) | (1 << 12) | (1 << 14) | (1 << 15);
-    LPC_SCT->EV[1].STATE = (1 << 1);
-    LPC_SCT->EV[1].CTRL = (0 << 0) | (1 << 12) | (1 << 14) | (0 << 15);
-    LPC_SCT->OUT[0].SET = (1 << 0);
-    LPC_SCT->OUT[0].CLR = (1 << 1);
-    LPC_SCT->LIMIT_L = 0x0003;
+
+    SctInit(LPC_SCT);    
+    //LPC_SCT->CONFIG |= 1;
+    SctConfig(LPC_SCT, SCT_CONFIG_32BIT_COUNTER);
+    //LPC_SCT->MATCHREL[0].U = ClockGetSystemClockRate();
+    SctSetMatchReload(LPC_SCT, SCT_MATCH_0, ClockGetSystemClockRate());
+    // LPC_SCT->EV[0].STATE = (1 << 0);
+    SctSetEventStateMask(LPC_SCT, SCT_EVT_0_VAL, SCT_STATE_0_BIT);
+    //LPC_SCT->EV[0].CTRL = (0 << 0) | (1 << 12) | (1 << 14) | (1 << 15);
+    SctSetEventControl(LPC_SCT, SCT_EVT_0_VAL, 
+        SCT_EV_CTRL_MATCHSEL(SCT_MATCH_0) | 
+        SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH) |
+        SCT_EV_CTRL_STATELD_LOAD | 
+        SCT_EV_CTRL_STATEV(SCT_STATE_1_VAL));
+    //LPC_SCT->EV[1].STATE = (1 << 1);
+    SctSetEventStateMask(LPC_SCT, SCT_EVT_1_VAL, SCT_STATE_1_BIT);
+    //LPC_SCT->EV[1].CTRL = (0 << 0) | (1 << 12) | (1 << 14) | (0 << 15);
+    SctSetEventControl(LPC_SCT, SCT_EVT_1_VAL, 
+        SCT_EV_CTRL_MATCHSEL(SCT_MATCH_0) | 
+        SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH) |
+        SCT_EV_CTRL_STATELD_LOAD | 
+        SCT_EV_CTRL_STATEV(SCT_STATE_0_VAL));
+    //LPC_SCT->OUT[0].SET = (1 << 0);
+    SctSetOutputSet(LPC_SCT, SCT_OUT_0_VALUE, SCT_EVT_0_BIT);
+    //LPC_SCT->OUT[0].CLR = (1 << 1);
+    SctSetOutputClear(LPC_SCT, SCT_OUT_0_VALUE, SCT_EVT_1_BIT);
+    //LPC_SCT->LIMIT_L = 0x0003;
+    SctSetLimitL(LPC_SCT, SCT_EVT_0_BIT | SCT_EVT_1_BIT);
     SctClearControl(LPC_SCT, SCT_CTRL_HALT_L);
 }
