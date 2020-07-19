001981a3acc3fef6413a82d68acdb35dedbb9f12
Squantor <squantmuts@gmail.com>
2020-07-19 14:02:37 +0200

    WIP deadtime pwm example

--- examples/LPC812_sct_example/src/deadtime_pwm.cpp
+++ examples/LPC812_sct_example/src/deadtime_pwm.cpp
@@ -32,6 +32,12 @@ SOFTWARE.
 #define TICKS_PER_S     (2)
 #define MAX_DUTY        (10)
 
+#define PWM_DUTY_0      (130)
+#define PWM_DUTY_1      (135)
+#define PWM_PERIOD      (180)
+
+
+
 volatile uint32_t ticks = 0;
 
 extern "C" 
@@ -48,12 +54,35 @@ void exampleSetup(void)
     ClockEnablePeriphClock(SYSCTL_CLOCK_SWM);
     ClockEnablePeriphClock(SYSCTL_CLOCK_IOCON);
     IoconPinSetMode(LPC_IOCON, IOCON_LED_0, PIN_MODE_INACTIVE);
+    IoconPinSetMode(LPC_IOCON, IOCON_LED_1, PIN_MODE_INACTIVE);
+    IoconPinSetMode(LPC_IOCON, IOCON_BUTTON, PIN_MODE_PULLUP);
     SwmMovablePinAssign(SWM_CTOUT_0_O, PIN_LED_0);
+    SwmMovablePinAssign(SWM_CTOUT_1_O, PIN_LED_1);
+    SwmMovablePinAssign(SWM_CTIN_0_I, PIN_BUTTON);
     ClockDisablePeriphClock(SYSCTL_CLOCK_IOCON);
     ClockDisablePeriphClock(SYSCTL_CLOCK_SWM);
     SysTick_Config(CLOCK_AHB / TICKS_PER_S);
-    
+       
     SctInit(LPC_SCT);
+    SctConfig(LPC_SCT, SCT_CONFIG_16BIT_COUNTER | SCT_CONFIG_AUTOLIMIT_L);
+    SctMatchCountL(LPC_SCT, SCT_MATCH_0, PWM_PERIOD);
+    SctMatchReloadL(LPC_SCT, SCT_MATCH_0, PWM_PERIOD);
+    SctMatchCountL(LPC_SCT, SCT_MATCH_1, PWM_DUTY_0);
+    SctMatchReloadL(LPC_SCT, SCT_MATCH_1, PWM_DUTY_0);
+    SctMatchCountL(LPC_SCT, SCT_MATCH_2, PWM_DUTY_1);
+    SctMatchReloadL(LPC_SCT, SCT_MATCH_2, PWM_DUTY_1);
+
+    SctSetEventStateMask(LPC_SCT, SCT_EVENT_0_VAL, SCT_STATE_0_BIT | SCT_STATE_1_BIT);
+    SctSetEventControl(LPC_SCT, SCT_EVENT_0_VAL, SCT_EV_CTRL_IOCOND(SCT_IOCOND_FALL) | SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_IO));
+    SctSetEventStateMask(LPC_SCT, SCT_EVENT_1_VAL, SCT_STATE_0_BIT | SCT_STATE_1_BIT);
+    SctSetEventControl(LPC_SCT, SCT_EVENT_1_VAL, SCT_EV_CTRL_IOCOND(SCT_IOCOND_RISE) | SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_IO));
+
+    SctSetEventStateMask(LPC_SCT, SCT_EVENT_2_VAL, SCT_STATE_0_BIT | SCT_STATE_1_BIT);
+    SctSetEventControl(LPC_SCT, SCT_EVENT_2_VAL, SCT_EV_CTRL_MATCHSEL(SCT_MATCH_1) | SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH));
+    SctSetEventStateMask(LPC_SCT, SCT_EVENT_3_VAL, SCT_STATE_0_BIT | SCT_STATE_1_BIT);
+    SctSetEventControl(LPC_SCT, SCT_EVENT_3_VAL, SCT_EV_CTRL_MATCHSEL(SCT_MATCH_2) | SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH));
+
+    
 }
 
 void exampleLoop(void)
