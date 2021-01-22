0de9608e755fbc42016df17afbd93eb472b3eb79
Squantor <squantmuts@gmail.com>
2021-01-22 16:59:27 +0100

    using newer libMcuLL for crystal oscillator control and enable crystal oscillator output on SCKOUT

--- software/LPC845M301HI48_blinky/src/nuclone_LPC845M301BD48.cpp
+++ software/LPC845M301HI48_blinky/src/nuclone_LPC845M301BD48.cpp
@@ -6,16 +6,35 @@
  */
 #include <nuclone_LPC845M301BD48.hpp>
 
+void crudeDelay(uint32_t iterations)
+{
+    for(uint32_t i = iterations; i > 0; i--)
+    {
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+    }
+}
+
 void boardInit(void)
 {
-    sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC | PDRUNCFG_SYSPLL);
-    sysconlEnableClocks(SYSCON, CLKCTRL0_IOCON | CLKCTRL0_GPIO0 | CLKCTRL0_SWM, CLKCTRL1_NONE);
+    sysconEnableClocks(SYSCON, CLKCTRL0_IOCON | CLKCTRL0_GPIO0 | CLKCTRL0_SWM, CLKCTRL1_NONE);
     ioconSetupPin(IOCON, IOCON_LED, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_XTAL_IN, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_XTAL_OUT, IOCON_MODE_INACTIVE);
-    ioconSetupPin(IOCON, IOCON_CLKOUT, IOCON_MODE_INACTIVE);
-    SwmMovablePinAssign(SWM0, SWM_CLKOUT, SWM_PORTPIN_CLKOUT);
     swmEnableFixedPin(SWM0, SWM_EN0_XTALIN | SWM_EN0_XTALOUT, SWM_EN1_NONE);
+    ioconSetupPin(IOCON, IOCON_CLKOUT, IOCON_MODE_INACTIVE);
+    sysconSysOscControl(SYSCON, SYSOSCCTRL_FREQ_1_20MHZ);
+    sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC);
+    crudeDelay(6000);
+    sysconExternalClockSelect(SYSCON, EXTCLKSEL_SYSOSC);
+    sysconMainClockSelect(SYSCON, MAINCLKSEL_EXTCLK);
+    SwmMovablePinAssign(SWM0, SWM_CLKOUT, SWM_PORTPIN_CLKOUT);
     gpioSetPinDIROutput(GPIO, PORT_LED, PIN_LED);
     gpioSetPinDIROutput(GPIO, PORT_LED, PIN_CLKOUT);
     gpioPinWrite(GPIO, PORT_LED, PIN_LED, 0);
