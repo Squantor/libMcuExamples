f7c61ec65a0aac5b7d64d35a1250339735a33613
Squantor <squantmuts@gmail.com>
2021-03-05 11:28:34 +0100

    initial start of libMcuLl tests for LPC812

---
+++ software/tests_LPC812/tests_LPC812.mk
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
+
+# project settings
+#
+# Version: 20201220
+
+# project settings
+MCU = LPC812M101DH20
+TARGET = MCU
+BOARD = nuclone_LPC812M101DH20_tests
+
+# project sources
+FILES += $(PROJECT)/src/main.cpp \
+$(PROJECT)/src/$(BOARD).cpp
+
+INCLUDES += -IlibMcuLL/inc -I$(PROJECT)/inc
+
+
