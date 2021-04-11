6b2582afb221722eb166bb22f788d2096d21a936
Squantor <squantmuts@gmail.com>
2021-04-11 14:44:21 +0200

    API updtae

--- software/tests_LPC845M301BD48/src/test_LPC845M301_teardown.cpp
+++ software/tests_LPC845M301BD48/src/test_LPC845M301_teardown.cpp
@@ -13,6 +13,6 @@
 
 MINUNIT_ADD(LPC845M301teardown, NULL, NULL)
 {
-    LPC845M301Teardown();
+    LPC845M301Teardown(testResults); // passing test state directly
     minUnitCheck(LPC845M301TeardownCorrect() == true);
 }
