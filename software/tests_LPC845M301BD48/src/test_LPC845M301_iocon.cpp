77085c831c3b327ed0113424f2f50f739875234d
Squantor <squantmuts@gmail.com>
2021-04-11 15:56:22 +0200

    completed pullup/down tests

--- software/tests_LPC845M301BD48/src/test_LPC845M301_iocon.cpp
+++ software/tests_LPC845M301BD48/src/test_LPC845M301_iocon.cpp
@@ -27,17 +27,19 @@ MINUNIT_SETUP(LPC845M301SetupIocon)
  */
 MINUNIT_ADD(LPC845M301IoconPull, LPC845M301SetupIocon, LPC845M301Teardown)
 {
-    // setup sensing GPIO to inputs and disable their pullups
     gpioSetPortDir(GPIO, PORT_TESTPIN_0_0, 0);
     ioconSetupPin(IOCON, IOCON_TESTPIN_0_0, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_TESTPIN_1_0, IOCON_MODE_INACTIVE);
-    // check if high (Default pullups on LPC)
     minUnitCheck(gpioPinRead(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0) == true);
     minUnitCheck(gpioPinRead(GPIO, PORT_TESTPIN_1_0, PIN_TESTPIN_1_0) == true);
-    // set iocon to pulldowns
-    // check if low
-    // set iocon to pullups
-    // check if high
+    ioconSetupPin(IOCON, IOCON_TESTPIN_0_1, IOCON_MODE_PULLDOWN);
+    ioconSetupPin(IOCON, IOCON_TESTPIN_1_1, IOCON_MODE_PULLDOWN);
+    minUnitCheck(gpioPinRead(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0) == false);
+    minUnitCheck(gpioPinRead(GPIO, PORT_TESTPIN_1_0, PIN_TESTPIN_1_0) == false);
+    ioconSetupPin(IOCON, IOCON_TESTPIN_0_1, IOCON_MODE_PULLUP);
+    ioconSetupPin(IOCON, IOCON_TESTPIN_1_1, IOCON_MODE_PULLUP);
+    minUnitCheck(gpioPinRead(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0) == true);
+    minUnitCheck(gpioPinRead(GPIO, PORT_TESTPIN_1_0, PIN_TESTPIN_1_0) == true);
 }
 
 /**
