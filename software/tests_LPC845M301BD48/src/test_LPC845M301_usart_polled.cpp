b62a64f2cc66f5227a9515e7ffb3ff498f969298
Squantor <squantmuts@gmail.com>
2021-05-24 15:09:32 +0200

    WIP uart TX/RX test

--- software/tests_LPC845M301BD48/src/test_LPC845M301_usart_polled.cpp
+++ software/tests_LPC845M301BD48/src/test_LPC845M301_usart_polled.cpp
@@ -18,15 +18,23 @@
 MINUNIT_SETUP(LPC845M301SetupUsart)
 {
     minUnitCheck(LPC845M301TeardownCorrect() == true);
-    sysconEnableClocks(SYSCON, CLKCTRL0_UART0 | CLKCTRL0_SWM | CLKCTRL0_IOCON, CLKCTRL1_NONE);
-    sysconEnableResets(SYSCON, RESETCTRL0_UART0 | RESETCTRL0_SWM | RESETCTRL0_IOCON, 0x00);
 }
 
 MINUNIT_ADD(LPC845M301UsartTxRx, LPC845M301SetupUsart, LPC845M301Teardown)
 {
-    // setup UART baud rate
-   
-    // enable UART
-    // Tx
-    // check Rx
+    sysconEnableClocks(SYSCON, CLKCTRL0_UART0 | CLKCTRL0_SWM | CLKCTRL0_IOCON, CLKCTRL1_NONE);
+    sysconEnableResets(SYSCON, RESETCTRL0_UART0 | RESETCTRL0_SWM | RESETCTRL0_IOCON, 0x00);
+    sysconPeripheralClockSelect(SYSCON, UART0CLKSEL, CLKSRC_MAIN);
+    SwmMovablePinAssign(SWM0, SWM_USART0_TXD, SWM_TESTPINT_0_0);
+    SwmMovablePinAssign(SWM0, SWM_USART0_RXD, SWM_TESTPINT_0_1);
+    // make sure we use a baud rate that is not an exact match
+    minUnitCheck(usartSetBaud(USART0, CLOCK_MAIN, 9600) == 9615);
+    usartSetConfig(USART0, DATALEN_8, PARITY_NONE, STOPLEN_1, 0);
+    usartTXEnable(USART0);
+    minUnitCheck(0 == 0);
+    usartTXDisable(USART0);
+    usartDisable(USART0);
+    SwmMovablePinAssign(SWM0, SWM_USART0_TXD, SWM_PORTPIN_Reset);
+    SwmMovablePinAssign(SWM0, SWM_USART0_RXD, SWM_PORTPIN_Reset);
+    sysconDisableClocks(SYSCON, CLKCTRL0_UART0 | CLKCTRL0_SWM | CLKCTRL0_IOCON, CLKCTRL1_NONE);
 }
