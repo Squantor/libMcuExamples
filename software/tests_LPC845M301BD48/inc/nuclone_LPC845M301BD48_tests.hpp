54fc456cf2c041d4678a19e642ddc1ecde4f35a1
Squantor <squantmuts@gmail.com>
2021-05-24 11:03:52 +0200

    added SWM references to the test pins

--- software/tests_LPC845M301BD48/inc/nuclone_LPC845M301BD48_tests.hpp
+++ software/tests_LPC845M301BD48/inc/nuclone_LPC845M301BD48_tests.hpp
@@ -32,16 +32,20 @@
 #define IOCON_TESTPIN_0_0   (IOCON_PIO0_1)
 #define PORT_TESTPIN_0_0    (GPIO_PORT0)
 #define PIN_TESTPIN_0_0     (1u)
+#define SWM_TESTPINT_0_0    (SWM_PORTPIN_P0_1)
 #define IOCON_TESTPIN_0_1   IOCON_PIO1_4
 #define PORT_TESTPIN_0_1    (GPIO_PORT1)
 #define PIN_TESTPIN_0_1     (4u)
+#define SWM_TESTPINT_0_1    (SWM_PORTPIN_P1_4)
 // test pin pair 1, connected to eachother via 1KOhm resistor
 #define IOCON_TESTPIN_1_0   (IOCON_PIO0_15)
 #define PORT_TESTPIN_1_0    (GPIO_PORT0)
 #define PIN_TESTPIN_1_0     (15u)
+#define SWM_TESTPIN_1_0     (SWM_PORTPIN_P0_15)
 #define IOCON_TESTPIN_1_1   (IOCON_PIO1_3)
 #define PORT_TESTPIN_1_1    (GPIO_PORT1)
 #define PIN_TESTPIN_1_1     (3u)
+#define SWM_TESTPIN_1_1     (SWM_PORTPIN_P1_3)
 
 void boardInit(void);
 
