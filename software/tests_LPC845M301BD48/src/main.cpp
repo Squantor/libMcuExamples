e11f87308f20d90ed13a8e24410526d393a37284
Squantor <squantmuts@gmail.com>
2021-05-24 11:10:00 +0200

    Added intro

--- software/tests_LPC845M301BD48/src/main.cpp
+++ software/tests_LPC845M301BD48/src/main.cpp
@@ -4,10 +4,8 @@
  * Copyright (c) 2021 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
-/*
- * This is simple example file which does do any I/O and thus
- * can be compiled and run on any Cortex-M MCU. However, to see
- * its effect, you'll need to use debugger.
+/**
+ * @brief main test entry point for LPC845 libMcuLL tests
  */
 #include <nuclone_LPC845M301BD48_tests.hpp>
 #include <MinUnit.h>
