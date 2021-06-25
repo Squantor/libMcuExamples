dc0f823beb199902b424708cf3c5647ceab49060
Squantor <squantmuts@gmail.com>
2021-06-25 12:47:01 +0200

    got API operational

--- software/tests_LPC824M201HI33/src/nuclone_LPC824M201HI33_tests.cpp
+++ software/tests_LPC824M201HI33/src/nuclone_LPC824M201HI33_tests.cpp
@@ -23,5 +23,20 @@ void crudeDelay(uint32_t iterations)
 
 void boardInit(void)
 {
-
+    sysconEnableClocks(SYSCON, CLKCTRL_IOCON | CLKCTRL_SWM);
+    // setup crystal oscillator to run core at 12MHz
+    ioconSetupPin(IOCON, IOCON_XTAL_IN, IOCON_MODE_INACTIVE);
+    ioconSetupPin(IOCON, IOCON_XTAL_OUT, IOCON_MODE_INACTIVE);
+    swmEnableFixedPin(SWM, SWM_EN0_XTALIN | SWM_EN0_XTALOUT);
+    //sysconSysOscControl(SYSCON, SYSOSCCTRL_FREQ_1_20MHZ);
+    //sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC);
+    // wait until crystal oscillator stabilizes
+    crudeDelay(6000);
+    //sysconExternalClockSelect(SYSCON, EXTCLKSEL_SYSOSC);
+    //sysconSysPllClockSelect(SYSCON, SYSPLLCLKSEL_EXTCLK);
+    //sysconMainClockPllSelect(SYSCON, MAINCLKPLLSEL_PREPLL);
+    // disable all unneeded clocks
+    sysconDisableClocks(SYSCON, CLKCTRL_IOCON | CLKCTRL_SWM);
+    // setup systick
+    SysTick_Config(CLOCK_AHB / TICKS_PER_S);
 }
