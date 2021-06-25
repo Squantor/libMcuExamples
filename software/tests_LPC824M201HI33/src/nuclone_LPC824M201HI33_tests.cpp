54a8b0571bca201cbc0b7c6f2c894535ad40dfff
Squantor <squantmuts@gmail.com>
2021-06-25 22:37:44 +0200

    testing syscon stuff, and clockout

--- software/tests_LPC824M201HI33/src/nuclone_LPC824M201HI33_tests.cpp
+++ software/tests_LPC824M201HI33/src/nuclone_LPC824M201HI33_tests.cpp
@@ -28,13 +28,16 @@ void boardInit(void)
     ioconSetupPin(IOCON, IOCON_XTAL_IN, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_XTAL_OUT, IOCON_MODE_INACTIVE);
     swmEnableFixedPin(SWM, SWM_EN0_XTALIN | SWM_EN0_XTALOUT);
-    //sysconSysOscControl(SYSCON, SYSOSCCTRL_FREQ_1_20MHZ);
-    //sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC);
+    sysconSysOscControl(SYSCON, SYSOSCCTRL_FREQ_1_20MHZ);
+    sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC);
     // wait until crystal oscillator stabilizes
     crudeDelay(6000);
-    //sysconExternalClockSelect(SYSCON, EXTCLKSEL_SYSOSC);
-    //sysconSysPllClockSelect(SYSCON, SYSPLLCLKSEL_EXTCLK);
-    //sysconMainClockPllSelect(SYSCON, MAINCLKPLLSEL_PREPLL);
+    sysconSysPllClockSelect(SYSCON, SYSPLLCLKSEL_SYSOSC);
+    sysconMainClockSelect(SYSCON, MAINCLKSEL_PLL_IN);
+    // setup clock output
+    sysconClkoutSource(SYSCON, CLKOUT_SYSOSC);
+    sysconClkoutDivider(SYSCON, 1);
+    SwmMovablePinAssign(SWM, SWM_CLKOUT_O, SWM_PORTPIN_P0_1);
     // disable all unneeded clocks
     sysconDisableClocks(SYSCON, CLKCTRL_IOCON | CLKCTRL_SWM);
     // setup systick
