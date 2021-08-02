f575e200ff634276e697d3eec898d42b7602f44e
Bart Bilos <squantmuts@gmail.com>
2021-08-02 13:21:46 +0200

    added I2C read not acknowledged test

--- software/tests_LPC844M201BD64/src/test_LPC844M201_i2c_polled.cpp
+++ software/tests_LPC844M201BD64/src/test_LPC844M201_i2c_polled.cpp
@@ -41,7 +41,36 @@ MINUNIT_ADD(LPC844M201I2CWriteNoAck, LPC844M201SetupI2C, LPC844M201Teardown)
         i2cStatus = i2cGetStatus(I2C0);
         i++;
     } while(I2C_STAT_MSTSTATE(i2cStatus) != I2C_STAT_MSSTATE_NACK_ADDRESS && i < 1000);
-    // timed out?
+    // timed out? and in a not acknowledged state?
+    minUnitCheck(i < 1000);
+    minUnitCheck(I2C_STAT_MSTSTATE(i2cStatus) == I2C_STAT_MSSTATE_NACK_ADDRESS);
+    swmDisableFixedPin(SWM, SWM_EN0_I2C0_SCL, SWM_EN1_NONE);
+    swmDisableFixedPin(SWM, SWM_EN0_I2C0_SDA, SWM_EN1_NONE);
+    sysconDisableClocks(SYSCON, CLKCTRL0_I2C0 | CLKCTRL0_SWM | CLKCTRL0_IOCON, CLKCTRL1_NONE);
+}
+
+MINUNIT_ADD(LPC844M201I2CReadNoAck, LPC844M201SetupI2C, LPC844M201Teardown)
+{
+    sysconEnableClocks(SYSCON, CLKCTRL0_I2C0 | CLKCTRL0_SWM | CLKCTRL0_IOCON, CLKCTRL1_NONE);
+    sysconEnableResets(SYSCON, RESETCTRL0_I2C0 | RESETCTRL0_SWM | RESETCTRL0_IOCON, RESETCTRL1_NONE);
+    ioconSetupPin(IOCON, IOCON_I2C_SCL_OUT, IOCON_I2CMODE_STD);
+    ioconSetupPin(IOCON, IOCON_I2C_SDA_OUT, IOCON_I2CMODE_STD);
+    swmEnableFixedPin(SWM, SWM_EN0_I2C0_SCL, SWM_EN1_NONE);
+    swmEnableFixedPin(SWM, SWM_EN0_I2C0_SDA, SWM_EN1_NONE);
+    sysconPeripheralClockSelect(SYSCON, I2C0CLKSEL, CLKSRC_MAIN);
+    i2cSetClockDivider(I2C0, 5);
+    i2cSetConfiguration(I2C0, I2C_CFG_MSTEN);
+    // write to address 0x7 with !W/R bit set
+    i2cSetMasterData(I2C0, 0x0F);
+    i2cSetMasterControl(I2C0, I2C_MSCTL_MSTSTART);
+    // waiting for status
+    uint32_t i2cStatus;
+    int i = 0;
+    do {
+        i2cStatus = i2cGetStatus(I2C0);
+        i++;
+    } while(I2C_STAT_MSTSTATE(i2cStatus) != I2C_STAT_MSSTATE_NACK_ADDRESS && i < 1000);
+    // timed out? and in a not acknowledged state?
     minUnitCheck(i < 1000);
     minUnitCheck(I2C_STAT_MSTSTATE(i2cStatus) == I2C_STAT_MSSTATE_NACK_ADDRESS);
     swmDisableFixedPin(SWM, SWM_EN0_I2C0_SCL, SWM_EN1_NONE);
