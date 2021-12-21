061c96f0546c3575de880ddcbd05343a10d2bb0e
Squantor <squantmuts@gmail.com>
2021-12-21 22:12:52 +0100

    WIP: getting LPC824M201DH20 blinky to compile

--- software/LPC824M201DH20_blinky/src/main.cpp
+++ software/LPC824M201DH20_blinky/src/main.cpp
@@ -8,23 +8,18 @@
  * Simple example blink program for the Nuclone LPC824M201DH20.
  * Using 12MHz Crystal + PLL as the clock source with systick.
  */
-#include <nuclone_LPC824M201DH20.hpp>
+#include <board.hpp>
 #include <mcu_ll.h>
 
 volatile int var;
 
-extern "C" 
-{
-    void SysTick_Handler(void)
-    {
-        GpioSetPinToggle(LPC_GPIO_PORT, 0, PIN_LED);
-    }
+extern "C" {
+void SysTick_Handler(void) { gpioSetPinToggle(LPC_GPIO_PORT, 0, PIN_LED); }
 }
 
-int main()
-{
-    boardInit();
-    while (1) {
-        var ^= 0x55;
-    }
+int main() {
+  boardInit();
+  while (1) {
+    var ^= 0x55;
+  }
 }
