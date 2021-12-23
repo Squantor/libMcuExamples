037dd268cec4f0059c2e1c0e0856072cce7e3887
Squantor <squantmuts@gmail.com>
2021-12-23 23:46:20 +0100

    functional LPC824M201DH20 blinky

--- software/LPC824M201DH20_blinky/inc/nuclone_LPC824M201DH20.hpp
+++ software/LPC824M201DH20_blinky/inc/nuclone_LPC824M201DH20.hpp
@@ -1,44 +1,28 @@
 /*
-MIT License
+SPDX-License-Identifier: MIT
 
-Copyright (c) 2019 Bart Bilos
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Copyright (c) 2021 Bart Bilos
+For conditions of distribution and use, see LICENSE file
 */
-#ifndef BOARD_HPP
-#define BOARD_HPP
+#ifndef NUCLONE_LPC824M201DH20_HPP
+#define NUCLONE_LPC824M201DH20_HPP
 
-#define CLOCK_MAIN_SOURCE   SYSCTL_MAINCLKSRC_PLLOUT
+#define CLOCK_MAIN_SOURCE SYSCTL_MAINCLKSRC_PLLOUT
 
-#define CLOCK_XTAL          (12000000u)
-#define CLOCK_EXT_IN        (0u)
-#define CLOCK_CPU           (30000000u)
-#define CLOCK_AHB           (30000000u)
-#define CLOCK_MAIN          (60000000u)
+#define CLOCK_XTAL (12000000u)
+#define CLOCK_EXT_IN (0u)
+#define CLOCK_CPU (30000000u)
+#define CLOCK_AHB (30000000u)
+#define CLOCK_MAIN (60000000u)
 
-#define IOCON_XTAL_IN       IOCON_PIO8
-#define IOCON_XTAL_OUT      IOCON_PIO9
+#define IOCON_XTAL_IN IOCON_PIO8
+#define IOCON_XTAL_OUT IOCON_PIO9
 
-#define IOCON_LED           IOCON_PIO23
-#define PIN_LED             (23u)
+#define IOCON_LED IOCON_PIO23
+#define PORT_LED GPIO_PORT0
+#define PIN_LED (23u)
 
-#define TICKS_PER_S         4
+#define TICKS_PER_S 4
 
 void boardInit(void);
 
--- software/LPC824M201DH20_blinky/src/nuclone_LPC824M201DH20.cpp
+++ software/LPC824M201DH20_blinky/src/nuclone_LPC824M201DH20.cpp
@@ -4,36 +4,45 @@
  * Copyright (c) 2021 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
-#include <nuclone_LPC824M201DH20.hpp>
-#include <mcu_ll.h>
+#include <board.hpp>
 
-void boardInit(void)
-{
-    ClockEnablePeriphClock(SYSCTL_CLOCK_SWM);
-    ClockEnablePeriphClock(SYSCTL_CLOCK_IOCON);
-    ClockEnablePeriphClock(SYSCTL_CLOCK_GPIO);
-    // set up all pin related things
-    SwmFixedPinEnable(SWM_FIXED_XTALIN, true);
-    SwmFixedPinEnable(SWM_FIXED_XTALOUT, true);
-    IoconPinSetMode(IOCON, IOCON_XTAL_IN, PIN_MODE_INACTIVE);
-    IoconPinSetMode(IOCON, IOCON_XTAL_OUT, PIN_MODE_INACTIVE);
-    IoconPinSetMode(IOCON, IOCON_LED, PIN_MODE_INACTIVE);
-    GpioSetPinState(LPC_GPIO_PORT, 0, IOCON_LED, false);
-    GpioSetPinDIROutput(LPC_GPIO_PORT, 0, PIN_LED);
+void crudeDelay(uint32_t iterations) {
+  for (uint32_t i = iterations; i > 0; i--) {
+    __NOP();
+    __NOP();
+    __NOP();
+    __NOP();
+    __NOP();
+    __NOP();
+    __NOP();
+    __NOP();
+  }
+}
 
-    ClockDisablePeriphClock(SYSCTL_CLOCK_SWM);
-
-    // setup system clocks
-    ClockSetPLLBypass(false, false);
-    SysctlPowerUp(SYSCTL_SLPWAKE_SYSOSC_PD);
-    ClockSetSystemPLLSource(SYSCTL_PLLCLKSRC_SYSOSC);
-    FmcSetFlashAccess(FLASHTIM_30MHZ_CPU);
-    SysctlPowerDown(SYSCTL_SLPWAKE_SYSPLL_PD);
-    ClockSetupSystemPLL(4, 1);
-    SysctlPowerUp(SYSCTL_SLPWAKE_SYSPLL_PD);
-    while (!ClockIsSystemPLLLocked());
-    ClockSetSysClockDiv(2);
-    ClockSetMainClockSource(SYSCTL_MAINCLKSRC_PLLOUT);
-
-    SysTick_Config(CLOCK_AHB / TICKS_PER_S);
+void boardInit(void) {
+  sysconEnableClocks(SYSCON, CLKCTRL_SWM | CLKCTRL_IOCON | CLKCTRL_GPIO);
+  ioconSetupPin(IOCON, IOCON_XTAL_IN, IOCON_MODE(PIN_INACTIVE));
+  ioconSetupPin(IOCON, IOCON_XTAL_OUT, IOCON_MODE(PIN_INACTIVE));
+  ioconSetupPin(IOCON, IOCON_LED, IOCON_MODE(PIN_INACTIVE));
+  swmEnableFixedPin(SWM, SWM_EN0_XTALIN | SWM_EN0_XTALOUT);
+  sysconDisableClocks(SYSCON, CLKCTRL_SWM | CLKCTRL_IOCON);
+  // setup system clocks
+  // ClockSetPLLBypass(false, false);
+  sysconSysOscControl(SYSCON, SYSOSCCTRL_BYPASS(0) | SYSOSCCTRL_FREQ_1_20MHZ);
+  sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC);
+  crudeDelay(6000);
+  sysconSysPllClockSelect(SYSCON, SYSPLLCLKSEL_SYSOSC);
+  FmcSetFlashAccess(FLASHTIM_30MHZ_CPU);
+  sysconPowerDisable(SYSCON, PDRUNCFG_SYSPLL);
+  sysconPllControl(SYSCON, 4, SYSPLLCTRL_POSTDIV_4);
+  //  SysctlPowerUp(SYSCTL_SLPWAKE_SYSPLL_PD);
+  sysconPowerEnable(SYSCON, PDRUNCFG_SYSPLL);
+  while (!sysconPllStatus(SYSCON))
+    ;
+  sysconMainClockDivider(SYSCON, 2);
+  // ClockSetMainClockSource(SYSCTL_MAINCLKSRC_PLLOUT);
+  sysconMainClockSelect(SYSCON, MAINCLKSEL_PLL_OUT);
+  gpioSetPinDIROutput(GPIO, PORT_LED, PIN_LED);
+  gpioPinWrite(GPIO, PORT_LED, PIN_LED, 0);
+  SysTick_Config(CLOCK_AHB / TICKS_PER_S);
 }
