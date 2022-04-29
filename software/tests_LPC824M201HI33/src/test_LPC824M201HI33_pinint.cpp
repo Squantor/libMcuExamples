89f7f86e6dbee565d3b15bc0cc01daa02d6728e9
Squantor <squantmuts@gmail.com>
2022-04-29 21:46:59 +0200

    started with level sensifitive test, updated teardown to include pinint

--- software/tests_LPC824M201HI33/src/test_LPC824M201HI33_pinint.cpp
+++ software/tests_LPC824M201HI33/src/test_LPC824M201HI33_pinint.cpp
@@ -37,7 +37,7 @@ MINUNIT_SETUP(LPC824M201HI33SetupPinint) {
 }
 
 /**
- * @brief
+ * @brief PININT tests for edge triggered interrupts
  */
 MINUNIT_ADD(LPC824M201HI33PinintEdges, LPC824M201HI33SetupPinint, LPC824M201HI33Teardown) {
   gpioSetPinDIRInput(GPIO, PORT_TESTPIN_0_1, PIN_TESTPIN_0_1);
@@ -61,9 +61,9 @@ MINUNIT_ADD(LPC824M201HI33PinintEdges, LPC824M201HI33SetupPinint, LPC824M201HI33
   minUnitCheck(pinIntIsrCounter == 1);
   // setup falling edge sensitivity
   NVIC_DisableIRQ(PININT0_IRQn);
+  pinintDisableIntHigh(PININT, PININT_CHAN0);
   pinintClearRiseStates(PININT, PININT_CHAN0);
   pinintClearFallStates(PININT, PININT_CHAN0);
-  pinintDisableIntHigh(PININT, PININT_CHAN0);
   pinintEnableIntLow(PININT, PININT_CHAN0);
   NVIC_EnableIRQ(PININT0_IRQn);
   // change pin
@@ -78,3 +78,43 @@ MINUNIT_ADD(LPC824M201HI33PinintEdges, LPC824M201HI33SetupPinint, LPC824M201HI33
   minUnitCheck(pinIntIsrCounter == 2);
   // Done
 }
+
+/**
+ * @brief PININT tests for edge triggered interrupts
+ */
+MINUNIT_ADD(LPC824M201HI33PinintLevels, LPC824M201HI33SetupPinint, LPC824M201HI33Teardown) {
+  gpioSetPinDIRInput(GPIO, PORT_TESTPIN_0_1, PIN_TESTPIN_0_1);
+  gpioSetPinDIROutput(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0);
+  gpioPinWrite(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0, 0);
+  delay_cycles(100);
+  // setup high level sensitivity
+  pinintSetPinModeLevel(PININT, PININT_CHAN0);
+  pinintClearRiseStates(PININT, PININT_CHAN0);
+  pinintClearFallStates(PININT, PININT_CHAN0);
+  pinintEnableIntHigh(PININT, PININT_CHAN0);
+  NVIC_EnableIRQ(PININT0_IRQn);
+  // change pin and trigger a few interrupts
+  gpioPinWrite(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0, 1);
+  delay_cycles(1000);
+  gpioPinWrite(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0, 0);
+  // count interrupts
+  minUnitCheck(pinIntIsrCounter > 3);
+
+  // setup low level sensitivity
+  NVIC_DisableIRQ(PININT0_IRQn);
+  gpioPinWrite(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0, 1);
+  pinintClearRiseStates(PININT, PININT_CHAN0);
+  pinintClearFallStates(PININT, PININT_CHAN0);
+  pinintDisableIntHigh(PININT, PININT_CHAN0);
+  pinintEnableIntLow(PININT, PININT_CHAN0);
+  NVIC_EnableIRQ(PININT0_IRQn);
+  // change pin and trigger a few interrupts
+  gpioPinWrite(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0, 0);
+  delay_cycles(1000);
+  gpioPinWrite(GPIO, PORT_TESTPIN_0_0, PIN_TESTPIN_0_0, 1);
+  // count interrupts
+  minUnitCheck(pinIntIsrCounter == 2);
+  // Done
+}
+
+// TODO tests for pattern matcher
