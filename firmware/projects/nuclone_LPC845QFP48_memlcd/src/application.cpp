020004e3992e9b6cd64a17785b2d5ea3b0b63cb6
Bart Bilos <squantmuts+git@gmail.com>
2024-12-29 17:02:44 +0100

    set block

--- firmware/projects/nuclone_LPC845QFP48_memlcd/src/application.cpp
+++ firmware/projects/nuclone_LPC845QFP48_memlcd/src/application.cpp
@@ -18,22 +18,44 @@ squLib::commandValueStack<8, commandConsole> commandValues;
 squLib::commandInterpreter<commandHandlers, commandValues, commandConsole> commandInterpreter;
 squLib::commandlineSimple<80, commandConsole, commandInterpreter> commandline;
 
+std::array<std::uint16_t, 64> pattern;
+
 void application::init() {
   commandConsole.print("LPC845 small nuclone test program\n");
+  pattern.fill(0x0000);
+  // fill pattern with simple XOR pattern
+  for (int x = 0; x < 32; x++) {
+    for (int y = 0; y < 32; y++) {
+      std::uint16_t mask = 0x01 << (x & 0xF);
+      std::size_t index = x / 16 + y;
+      if ((x ^ y) & 0x40)
+        pattern[index] = pattern[index] | mask;
+    }
+  }
 }
 void application::progress() {
   static std::uint32_t currentTicksSeconds = ticks;
   static std::uint32_t seconds = 0;
   static std::uint32_t currentTicksFaster = ticks;
   static std::uint32_t faster = 0;
+  static std::uint32_t xCoord = 0;
+  static std::uint32_t yCoord = 0;
 
   if (currentTicksSeconds + 100 < ticks) {
-    memlcdDriver.sendVcom();
+    memlcdDriver.toggleVcom();
     seconds++;
     currentTicksSeconds = ticks;
+    xCoord += 3;
+    if (xCoord > display.getXSize())
+      xCoord -= display.getXSize();
+    yCoord += 5;
+    if (yCoord > display.getYSize())
+      yCoord -= display.getYSize();
+    display.fill(xCoord, yCoord, xCoord + 20, yCoord + 20, 0);
+    display.update();
   }
 
-  if (currentTicksFaster + 50 < ticks) {
+  if (currentTicksFaster + 40 < ticks) {
     for (std::uint32_t x = 0; x < libMcuDrv::memlcd::LS013B4DN04::maxX; x++) {
       for (std::uint32_t y = 0; y < libMcuDrv::memlcd::LS013B4DN04::maxX; y++) {
         display.setPixel(x, y, (x * x + y * y + faster) & 64);
