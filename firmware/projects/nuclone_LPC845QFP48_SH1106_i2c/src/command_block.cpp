ce295b97a3bff9ba827810f1c7584295259fc13b
Bart Bilos <squantmuts+git@gmail.com>
2025-12-19 10:22:10 +0100

    working block write command

--- firmware/projects/nuclone_LPC845QFP48_SH1106_i2c/src/command_block.cpp
+++ firmware/projects/nuclone_LPC845QFP48_SH1106_i2c/src/command_block.cpp
@@ -11,21 +11,24 @@
 #include <commands.hpp>
 #include <application.hpp>
 
-std::array<std::uint8_t, 32> pattern_buffer;
+std::array<std::uint8_t, 16> pattern_buffer;
 
 squLib::results block(std::span<const char> commandLine) {
   (void)commandLine;
-  if (commandValues.size() < 3) {
-    commandConsole.print("Needs 3 elements on the stack, X Y coordinates and a pattern\n");
+  if (commandValues.size() < 4) {
+    commandConsole.print("Needs 4 elements from top to bottom: pattern, length, column, row\n");
     return squLib::results::error;
   }
-  std::int32_t x, y, pattern;
-  commandValues.pop(x);
-  commandValues.pop(y);
+  std::int32_t col, row, length, pattern;
   commandValues.pop(pattern);
+  commandValues.pop(length);
+  commandValues.pop(col);
+  commandValues.pop(row);
   pattern_buffer.fill(static_cast<std::uint8_t>(pattern));
-  display.SendData(pattern_buffer);
+  display.SetAddress(static_cast<uint32_t>(col), static_cast<uint32_t>(row));
+  display.SendData(std::span<uint8_t>(pattern_buffer).subspan(0, length));
   return squLib::results::ok;
 }
 
-squLib::commandHandler block_handler{"block", "writes to x and y a block with pattern p\n", block};
+squLib::commandHandler block_handler{
+  "block", "writes to column and row a block with pattern p of length l\n stack format pattern, length, column, row\n", block};
